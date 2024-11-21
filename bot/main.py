d9cd907bbc441dd1b60017107373aa32cabf68cc
KohiiTM <jamesandallen.dev@gmail.com>
2024-11-20 20:00:12 -0800

    fixes

--- bot/main.py
+++ bot/main.py
@@ -8,11 +8,10 @@ TOKEN = os.getenv("DISCORD_TOKEN")
 
 class Client(discord.Client):
     async def on_ready(self):
-        print(f'Logged in as {self.user}!')+
+        print(f'Logged in as {self.user}!')
 
-intens = discord.Intents.default()
-intens.message_content = True
+intents = discord.Intents.default()
+intents.message_content = True
 
-client = Client(intents=intens)
-client.run('DISCORD_TOKEN')
-
+client = Client(intents=intents)
+client.run(TOKEN)
