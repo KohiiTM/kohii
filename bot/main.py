996f58fc075fccbfb35a3f33f87509302305363e
KohiiTM <jamesandallen.dev@gmail.com>
2024-11-20 20:27:21 -0800

    additions, reworked bot to use commands.Bot class

--- bot/main.py
+++ bot/main.py
@@ -1,4 +1,5 @@
 import discord
+from discord.ext import commands
 import os
 from dotenv import load_dotenv
 
@@ -6,12 +7,26 @@ load_dotenv()
 
 TOKEN = os.getenv("DISCORD_TOKEN")
 
-class Client(discord.Client):
-    async def on_ready(self):
-        print(f'Logged in as {self.user}!')
-
+#intents
 intents = discord.Intents.default()
 intents.message_content = True
 
-client = Client(intents=intents)
-client.run(TOKEN)
+bot = commands.Bot(command_prefix='-', intents=intents)
+
+@bot.event
+async def on_ready(self):
+    print(f'Logged in as {bot.user}!') # Confirm the bot is online
+    await bot.tree.sync()
+    print("Slash commands synced")
+
+async def on_message(self, message):
+    if message.author == self.user:
+        return
+
+    if message.content.startswith('hello'):
+        await message.channel.send(f'yo {message.author.display_name}')
+
+
+
+
+bot.run(TOKEN)
